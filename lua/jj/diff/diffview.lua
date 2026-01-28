c750c397649591aebc37b777184ea74a70874583
NicolasGB <ngou0210@gmail.com>
2026-01-28 14:20:34 +0100

    fix(diff): Correctly compare against `@-` instead of `HEAD~1` when diffing the current buffer

--- lua/jj/diff/diffview.lua
+++ lua/jj/diff/diffview.lua
@@ -32,13 +32,11 @@ diff.register_backend("diffview", {
 		end
 
 		-- Extract the commit id from opts.rev
-		local commit_id = "HEAD~1"
-		if opts.rev then
-			local t_commit_id = utils.get_commit_id(opts.rev)
-			if t_commit_id == nil then
-				return
-			end
-			commit_id = t_commit_id
+		local revset = opts.rev or "@-"
+
+		local commit_id = utils.get_commit_id(revset)
+		if not commit_id then
+			return
 		end
 
 		vim.cmd(string.format("DiffviewOpen %s -- %%", commit_id))
